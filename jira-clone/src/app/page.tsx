e5c099a56e35cb2a3327ba9dd16643b36acb4bfa
Sanskriti Bhatt <sanskritibhatt04@gmail.com>
2025-04-03 18:09:42 +0530

    Initial Commit

---
+++ jira-clone/src/app/page.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { TestComp } from "@/features/test";
+import { Input } from "@/components/ui/input";
+
+export default function Home() {
+  return ( 
+    <div className="">
+      <Input />
+      <Button size="xs" >
+        Primary
+      </Button>
+      <Button variant="secondary">
+        Secondary
+      </Button>
+      <Button variant="destructive">
+        Destructive
+      </Button>
+      <Button variant="ghost">
+        Ghost
+      </Button>
+      <Button variant="muted">
+        Muted
+      </Button>
+      <Button variant="teritary">
+      Teritary
+      </Button>
+    </div>
+   );
+}
---
+++ jira-clone/src/features/test.tsx
@@ -0,0 +1,7 @@
+export const TestComp=() =>{
+    return (
+        <div> 
+            Hellow test comp 
+        </div>
+    )
+}
