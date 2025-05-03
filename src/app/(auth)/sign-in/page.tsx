7255d169b9574221b3d468174515299c2654c7e7
Happyesss <22csaiml002@jssaten.ac.in>
2025-05-03 23:48:14 +0530

    Refactor authentication pages to use async user retrieval

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,14 @@
-"use client";
-import { SignInCard } from "@/features/auth/components/sign-in-card";
-const SignInPage= () =>{   
-    return <SignInCard />
- };
+import { redirect } from "next/navigation";
 
- export default SignInPage ; 
+import { getCurrent } from "@/features/auth/actions";
+import { SignInCard } from "@/features/auth/components/sign-in-card";
+
+const SignInPage = async () => {
+  const user = await getCurrent();
+
+  if (user) redirect("/");
+
+  return <SignInCard />;
+};
+
+export default SignInPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,13 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { UserButton } from "@/features/auth/components/user-button";
+import { getCurrent } from "@/features/auth/actions";
+import { redirect } from "next/navigation";
 
-import { useCurrent } from "@/features/auth/api/use-current";
-import { Button } from "@/components/ui/button";
-import { useLogout } from "@/features/auth/api/use-logout";
-
-export default function Home() {
-  const router = useRouter();
-  const { data, isLoading } = useCurrent();
-  const { mutate } = useLogout();
-
-  useEffect(() => {
-    if (!data && !isLoading) {
-      router.push("/sign-in");
-    }
-  }, [data]);
-
+export default async function Home() {
+  const user = await getCurrent();
+  if (!user) redirect("/sign-in");
   return (
     <div>
-      Only visible to authorized users.
-      <Button onClick={()=> mutate()}>
-        Logout
-      </Button>
+      <UserButton />
     </div>
   );
 }
